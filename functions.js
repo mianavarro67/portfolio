758dec92360da8fb04a39b85bee21d9d90beb9e5
Mia <navarromn@vcu.edu>
2019-12-17 11:18:55 -0500

    final

---
+++ functions.js
@@ -0,0 +1,19 @@
+//modal initiator
+// var btn = document.getElementsByClassName('button');
+// var i;
+//
+// for (i = 0; i < btn.length; i++) {
+//   btn[i].addEventListener("click", function() {
+//     var modal = this.nextElementSibling;
+//     modal.classList.toggle("modal-open");
+//   });
+// }
+
+// var modal = document.getElementById('mymodal');
+//when user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target = modal) {
+//     console.log('hello');
+//     modal.classList.toggle("modal-open");
+//   }
+// }
